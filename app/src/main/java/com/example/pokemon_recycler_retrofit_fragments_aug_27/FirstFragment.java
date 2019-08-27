17196e10e4cc919441659b1dc6028059972116e7
nmuppuri <narendramuppuri@gmail.com>
2019-08-27 13:16:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/pokemon_recycler_retrofit_fragments_aug_27/FirstFragment.java
@@ -0,0 +1,25 @@
+package com.example.pokemon_recycler_retrofit_fragments_aug_27;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+}
